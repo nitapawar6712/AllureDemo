f0f25aef471ea099c91766e7ff0ea8f65e2bcd5e
Nita <nita.pawr6712@gmail.com>
2025-09-24 11:23:43 +0530

    Allure1

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import io.qameta.allure.Description;
+
+public class AppTest {
+
+    @Test
+    @Description("Simple test to check addition")
+    public void testAddition() {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        assertEquals(sum, 15, "Sum should be 15");
+    }
+     @Test
+    @Description("Simple test to check addition")
+    public void testSub() {
+        int a = 20;
+        int b = 10;
+        int diff = a - b;
+        assertEquals(diff, 10, "Sum should be 10");
+    }
+}
